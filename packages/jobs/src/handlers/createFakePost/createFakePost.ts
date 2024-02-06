d641d8a7b53643108cc86effff916d999d77b616
Cedric <mcdougal.ce@gmail.com>
2024-02-06 07:26:59 -0500

    temp

--- packages/jobs/src/handlers/createFakePost/createFakePost.ts
+++ packages/jobs/src/handlers/createFakePost/createFakePost.ts
@@ -5,29 +5,27 @@ import { getOrCreateUserForSession } from '@/domain/user/server';
 import { v4 as uuidv4 } from 'uuid';
 
 export default async (): Promise<void> => {
-  if (Math.random()) {
-    const sessionUser = await getOrCreateUserForSession({
-      where: { sessionId: uuidv4() },
+  const sessionUser = await getOrCreateUserForSession({
+    where: { sessionId: uuidv4() },
+  });
+
+  const postedAt = new Date();
+  const updatedAt = postedAt;
+  const expiresAt = getExpiresAt(updatedAt);
+
+  const message = await createChatCompletion(
+    `Say an opinion about a random pop culture reference`
+  );
+
+  if (message) {
+    await createPost({
+      data: {
+        authorId: sessionUser.id,
+        expiresAt,
+        postedAt,
+        text: message,
+        updatedAt,
+      },
     });
-
-    const postedAt = new Date();
-    const updatedAt = postedAt;
-    const expiresAt = getExpiresAt(updatedAt);
-
-    const message = await createChatCompletion(
-      `Say an opinion about a random pop culture reference`
-    );
-
-    if (message) {
-      await createPost({
-        data: {
-          authorId: sessionUser.id,
-          expiresAt,
-          postedAt,
-          text: message,
-          updatedAt,
-        },
-      });
-    }
   }
 };
